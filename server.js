747e86d68ca31fd3a160075c2a18543546088633
Josh Brian Bugarin <jofe.bugarin.up@phinmaed.com>
2024-09-03 13:56:07 +0800

    Added API routes, MongoDB extension and Nodemon

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
 
 
-//API route
-app.get('', (req, res) =>{
+//API routes
+
+//Read
+app.get('/', (req, res) =>{
 	res.send("Hello API")
 })
 
-app.listen(3000, () => {
-	console.log("Node API app is running on port 3000")
+app.get('/blog', (req, res) =>{
+	res.send("Hello APIsdsdsds")
 })
+
+//Create
+
+//Update
+
+//Delete
+
+//Database Connection
+mongoose.set("strictQuery", false);
+mongoose.
+connect('mongodb+srv://admin:pass123@chromacyapi.1fqug.mongodb.net/Node-API?retryWrites=true&w=majority&appName=ChromaCyAPI')
+.then(() => {
+	//App Start
+	console.log("Connected to MongoDB")
+    app.listen(3000, () => {
+	    console.log("Node API app is running on port 3000")
+    });
+})
+.catch((error) => {
+	console.log(error)
+})
+
